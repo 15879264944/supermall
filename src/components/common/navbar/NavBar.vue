fa7b53685f431294740a4bb238339590e59225de
lulang <2268050526@qq.com>
2021-03-05 13:31:49 +0800

    一次

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,31 @@
+<template>
+<!-- 首页导航栏的封装 -->
+    <div class="nav-bar">
+        <div class="left"><slot name="left"></slot></div>
+        <div class="center"><slot name="center"></slot></div>
+        <div class="right"><slot name="right"></slot></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'navBar'
+}
+</script>
+<style>
+    .nav-bar{
+        display: flex;
+        height: 44px;
+        line-height: 44px;
+        text-align: center;
+        box-shadow: 0 1px 1px rgba(100, 100, 100, .1);
+    }
+    .left, .right{
+        width: 60px;
+        /* background-color: red; */
+    }
+    .center{
+        flex: 1;
+        /* background-color: blue; */
+    }
+</style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,14 +1,54 @@
 <template>
-    <div>
-        home
+    <div id="home">
+        <!-- 导航栏 -->
+        <nav-bar class="home-nav">
+            <div slot="center">购物街</div>
+        </nav-bar>
+        <!-- 轮播图 -->
+        <home-swiper :banners='banners'></home-swiper>
+        <!-- 组件 -->
+        <home-recommend-view :recommends='recommends'></home-recommend-view>
     </div>
 </template>
 
 <script>
+import NavBar from 'components/common/navbar/NavBar';
+import HomeSwiper from './childComps/HomeSwiper'
+import HomeRecommendView from './childComps/HomeRecommendView'
+
+
+
+import { getHomeMultidata } from 'network/home';
+
+
+
 export default {
-    
+    name:'Home',
+    components:{
+        NavBar,
+        HomeSwiper,
+        HomeRecommendView
+        
+    },
+    data() {
+        return{
+            banners:[],
+            recommends:[]
+        }
+    },
+    created(){
+        //1.请求多给数据
+        getHomeMultidata().then(res => {
+            //console.log(res)
+            this.banners = res.data.banner.list;
+            this.recommends = res.data.recommend.list;
+        });
+    }
 }
 </script>
 <style>
-    
+    .home-nav{
+        background-color: var(--color-tint);
+        color:#fff
+    }
 </style>
